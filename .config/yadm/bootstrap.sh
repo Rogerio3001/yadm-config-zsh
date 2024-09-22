74f3544bb77d755bd76affd04a5b5614cd4cd71b
rogerio3001 <rogeriovinicios3001@gmail.com>
2024-09-22 14:10:01 -0300

    Adiciona instalacao opcional do nvm

--- .config/yadm/bootstrap.sh
+++ .config/yadm/bootstrap.sh
@@ -2,6 +2,7 @@
 
 # Defina variáveis de caminho
 OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
+NVM_DIR="$HOME/.nvm"
 
 # Instalar Oh My Zsh apenas se ele ainda não estiver instalado
 if [ ! -d "$OH_MY_ZSH_DIR" ]; then
@@ -26,6 +27,29 @@ fi
 if ! grep -q "zsh-autosuggestions" "$HOME/.zshrc"; then
   echo "Adicionando plugins ao .zshrc..."
   sed -i '/^plugins=/ s/(/(zsh-autosuggestions /' "$HOME/.zshrc"
+fi
+
+# Perguntar ao usuário se deseja instalar o NVM
+read -p "Deseja instalar o NVM? (y/n): " install_nvm
+if [ "$install_nvm" = "y" ]; then
+  # Instalar NVM apenas se ele ainda não estiver instalado
+  if [ ! -d "$NVM_DIR" ]; then
+    echo "Instalando NVM..."
+    
+    # Baixar e instalar NVM
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    
+    # Adicionar NVM ao .zshrc se ainda não estiver presente
+    if ! grep -q "NVM_DIR" "$HOME/.zshrc"; then
+      echo "Adicionando NVM ao .zshrc..."
+      echo 'export NVM_DIR="$HOME/.nvm"' >> "$HOME/.zshrc"
+      echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> "$HOME/.zshrc"
+    fi
+  else
+    echo "NVM já instalado."
+  fi
+else
+  echo "Instalação do NVM pulada."
 fi
 
 # Reiniciar o shell para aplicar as alterações
