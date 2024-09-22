2608cc494c12a1b7cfd82e1f00af8160c1ab9876
rogerio3001 <rogeriovinicios3001@gmail.com>
2024-09-22 14:01:12 -0300

    Corrige erro de sintaxe no bash

--- .config/yadm/bootstrap.sh
+++ .config/yadm/bootstrap.sh
@@ -19,27 +19,6 @@ ZSH_CUSTOM="$OH_MY_ZSH_DIR/custom"
 
 # Instalar autosuggestions
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestio#!/bin/bash
-
-# Defina variáveis de caminho
-OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
-
-# Instalar Oh My Zsh apenas se ele ainda não estiver instalado
-if [ ! -d "$OH_MY_ZSH_DIR" ]; then
-  echo "Instalando Oh My Zsh..."
-  
-  # Instalar Oh My Zsh sem trocar o shell automaticamente
-  RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
-  echo "Oh My Zsh já instalado."
-fi
-
-# Instalar plugins do Oh My Zsh
-echo "Instalando plugins do Oh My Zsh..."
-ZSH_CUSTOM="$OH_MY_ZSH_DIR/custom"
-
-# Instalar autosuggestions
-if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 fi
 
@@ -56,4 +35,3 @@ fi
 
 # Reiniciar o shell para aplicar as alterações
 echo "Bootstrap concluído! Por favor, reinicie seu terminal ou execute 'exec zsh' para aplicar as mudanças."
-
